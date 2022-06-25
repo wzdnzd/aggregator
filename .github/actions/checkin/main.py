049f3c550565c8fbbf83492fbbc969a60a2a301f
wzdnzd <hanxi2014@gmail.com>
2022-06-25 11:30:43 +0800

    remove base64 encoding and decoding

--- .github/actions/checkin/main.py
+++ .github/actions/checkin/main.py
@@ -9,36 +9,32 @@ import warnings
 import urllib
 import urllib.request
 import urllib.parse
-import base64
 import multiprocessing
 import os
 import ssl
 import json
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 HEADER = {
-    "user-agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.54",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37",
     "accept": "application/json, text/javascript, */*; q=0.01",
     "accept-language": "zh-CN,zh;q=0.9",
     "dnt": "1",
     "Connection": "keep-alive",
     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "x-requested-with": "XMLHttpRequest"
+    "x-requested-with": "XMLHttpRequest",
 }
 
 CTX = ssl.create_default_context()
 CTX.check_hostname = False
 CTX.verify_mode = ssl.CERT_NONE
 
-RETRY_NUM = 5
-
 PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def extract_domain(url):
-    if not url:
+    if not url or not re.match("^(https?:\/\/(([a-zA-Z0-9]+-?)+\.)+[a-zA-Z]+)(:\d+)?(\/.*)?(\?.*)?(#.*)?$", url):
         return ""
 
     start = url.find("//")
@@ -47,25 +43,22 @@ def extract_domain(url):
 
     end = url.find("/", start + 2)
     if end == -1:
-        end = len(url) - 1
+        end = len(url)
 
-    return url[start + 2:end]
+    return url[:end]
 
 
 def login(url, params, headers, retry):
     try:
-        data = urllib.parse.urlencode(params).encode(encoding='UTF8')
+        data = urllib.parse.urlencode(params).encode(encoding="UTF8")
 
-        request = urllib.request.Request(url,
-                                         data=data,
-                                         headers=headers,
-                                         method='POST')
+        request = urllib.request.Request(url, data=data, headers=headers, method="POST")
 
         response = urllib.request.urlopen(request, context=CTX)
-        print(response.read().decode('unicode_escape'))
+        print(response.read().decode("unicode_escape"))
 
         if response.getcode() == 200:
-            return response.getheader('Set-Cookie')
+            return response.getheader("Set-Cookie")
 
     except Exception as e:
         print(str(e))
@@ -75,17 +68,16 @@ def login(url, params, headers, retry):
             login(url, params, headers, retry)
 
         print("[LoginError] URL: {}".format(extract_domain(url)))
-        return ''
+        return ""
 
 
 def checkin(url, headers, retry):
     try:
-        request = urllib.request.Request(url, headers=headers, method='POST')
+        request = urllib.request.Request(url, headers=headers, method="POST")
 
         response = urllib.request.urlopen(request, context=CTX)
-        data = response.read().decode('unicode_escape')
-        print("[CheckInFinished] URL: {}\t\tResult:{}".format(
-            extract_domain(url), data))
+        data = response.read().decode("unicode_escape")
+        print("[CheckInFinished] URL: {}\t\tResult:{}".format(extract_domain(url), data))
 
     except Exception as e:
         print(str(e))
@@ -100,10 +92,10 @@ def checkin(url, headers, retry):
 def get_cookie(text):
     regex = "(__cfduid|uid|email|key|ip|expire_in)=(.+?);"
     if not text:
-        return ''
+        return ""
 
     content = re.findall(regex, text)
-    cookie = ';'.join(['='.join(x) for x in content]).strip()
+    cookie = ";".join(["=".join(x) for x in content]).strip()
 
     return cookie
 
@@ -112,34 +104,25 @@ def config_load(filename):
     if not os.path.exists(filename) or os.path.isdir(filename):
         return
 
-    config = open(filename, 'r').read()
+    config = open(filename, "r").read()
     return json.loads(config)
 
 
 def flow(domain, params, headers):
-    domain = base64.b64decode(domain).decode("utf8")
-    print('start to checkin, domain: {}'.format(domain))
-
-    domain = domain.strip()
-    regex = "(?i)^(https?:\\/\\/)?(www.)?([^\\/]+\\.[^.]*$)"
-    flag = re.search(regex, domain)
-
-    if not flag:
+    domain = extract_domain(domain.strip())
+    if not domain:
+        print("cannot checkin because domain is invalidate")
         return False
 
-    login_url = domain + base64.b64decode(
-        params.get("login", "L2F1dGgvbG9naW4=")).decode("utf8")
-    checkin_url = domain + base64.b64decode(
-        params.get("checkin", "L3VzZXIvY2hlY2tpbg==")).decode("utf8")
+    print("start to checkin, domain: {}".format(domain))
+    login_url = domain + params.get("login", "/auth/login")
+    checkin_url = domain + params.get("checkin", "/user/checkin")
     headers["origin"] = domain
     headers["referer"] = login_url
 
-    user_info = {
-        "email": base64.b64decode(params['email']),
-        "passwd": base64.b64decode(params['passwd'])
-    }
+    user_info = {"email": params.get("email", ""), "passwd": params.get("passwd", "")}
 
-    text = login(login_url, user_info, headers, RETRY_NUM)
+    text = login(login_url, user_info, headers, 3)
     if not text:
         return False
 
@@ -150,29 +133,24 @@ def flow(domain, params, headers):
     headers["referer"] = domain + "/user"
     headers["cookie"] = cookie
 
-    checkin(checkin_url, headers, RETRY_NUM)
+    checkin(checkin_url, headers, 3)
 
 
 def wrapper(args):
-    flow(args["domain"], args["param"], HEADER)
+    flow(args.get("domain", ""), args.get("param", {}), HEADER)
 
 
 def main():
-    config = config_load(os.path.join(PATH, 'config.json'))
-    if "retry" in config and config["retry"] > 0:
-        RETRY_NUM = int(config["retry"])
+    config = config_load(os.path.join(PATH, "config.json"))
+    params = config.get("domains", [])
 
-    params = config["domains"]
-    for args in params:
-        wrapper(args)
+    cpu_count = multiprocessing.cpu_count()
+    num = len(params) if len(params) <= cpu_count else cpu_count
 
-    # cpu_count = multiprocessing.cpu_count()
-    # num = len(params) if len(params) <= cpu_count else cpu_count
-
-    # pool = multiprocessing.Pool(num)
-    # pool.map(wrapper, params)
-    # pool.close()
+    pool = multiprocessing.Pool(num)
+    pool.map(wrapper, params)
+    pool.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
