e15d42bef3b1fede65d990f1cef7c25d7e63a76b
Jason <47520063+JasonLeeForOnly@users.noreply.github.com>
2024-03-23 00:23:56 +0800

    Update check_links.py

--- aggregate/subscribe/check_links.py
+++ aggregate/subscribe/check_links.py
@@ -9,25 +9,35 @@ def check_link(link):
         else:
             return False
     except requests.exceptions.RequestException as error:
-        print(f"Request failed for link {link.strip()}: {error}")
+        print(f"Request failed for link {link}: {error}")
         return False
 
+def split_links(links):
+    valid_links = []
+    for link in links:
+        splitted_links = link.split('http')
+        # Ignore the first empty element and add 'http' back to every link
+        valid_links.extend(['http' + l if i != 0 else l for i, l in enumerate(splitted_links)])
+    return valid_links
+    
 def check_links(file):
     with open(file, "r") as f:
         links = f.readlines()
 
+    links = split_links(links)
     valid_links = []
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        future_to_link = {executor.submit(check_link, link): link for link in links}
+        future_to_link = {executor.submit(check_link, link.strip()): link.strip() for link in links}
         for future in concurrent.futures.as_completed(future_to_link):
             link = future_to_link[future]
             try:
                 result = future.result()
                 if result == True:
-                    valid_links.append(link)
+                    valid_links.append(link + 'n')
             except Exception as exc:
-                print(f"{link.strip()} generated an exception: {exc}.")
-    
+                print(f"{link} generated an exception: {exc}.")
+        
     with open(file, "w") as f:
         f.writelines(valid_links)
 
