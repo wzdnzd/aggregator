22e280bf3d7f78a774aeb8659b0651e9b162683c
wzdnzd <hanxi2014@gmail.com>
2024-05-16 19:30:43 +0800

    support push to github gist
    Former-commit-id: 6506460c771523c0e70371b35dc45eac557175e9

--- subscribe/push.py
+++ subscribe/push.py
@@ -304,7 +304,7 @@ class PushToDrift(PushToPastefy):
     def __init__(self, token: str) -> None:
         super().__init__(token=token)
         self.name = "drift"
-        self.api_address = "https://pastebin.enjoyit.ml/api/file"
+        self.api_address = "https://paste.ding.free.hr/api/file"
 
     def raw_url(self, push_conf: dict) -> str:
         if not push_conf or type(push_conf) != dict:
@@ -377,7 +377,81 @@ class PushToLocal(PushTo):
         return f"{utils.FILEPATH_PROTOCAL}{filepath}"
 
 
-PUSHTYPE = Enum("PUSHTYPE", ("pastebin.enjoyit.ml", "pastefy.ga", "paste.gg", "imperialb.in"))
+class PushToGist(PushTo):
+    def __init__(self, token: str) -> None:
+        super().__init__(token=token)
+
+        self.name = "gist"
+        self.api_address = "https://api.github.com/gists"
+        self.method = "PATCH"
+
+    def validate(self, push_conf: dict) -> bool:
+        if not isinstance(push_conf, dict):
+            return False
+
+        gistid = push_conf.get("gistid", "")
+        filename = push_conf.get("filename", "")
+
+        return "" != self.token.strip() and "" != gistid.strip() and "" != filename.strip()
+
+    def _generate_payload(self, content: str, push_conf: dict) -> tuple[str, str, dict]:
+        gistid = push_conf.get("gistid", "")
+        filename = push_conf.get("filename", "")
+
+        url = f"{self.api_address}/{gistid}"
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "User-Agent": utils.USER_AGENT,
+        }
+
+        data = json.dumps({"files": {filename: {"content": content, "filename": filename}}}).encode("UTF8")
+        return url, data, headers
+
+    def _is_success(self, response: HTTPResponse) -> bool:
+        return response and response.getcode() == 200
+
+    def filter_push(self, push_conf: dict) -> dict:
+        if not self.token or not isinstance(push_conf, dict):
+            return {}
+
+        return {
+            k: v
+            for k, v in push_conf.items()
+            if k and isinstance(v, dict) and v.get("gistid", "") and v.get("filename", "")
+        }
+
+    def raw_url(self, push_conf: dict) -> str:
+        if not push_conf or type(push_conf) != dict:
+            return ""
+
+        username = utils.trim(push_conf.get("username", ""))
+        gistid = utils.trim(push_conf.get("gistid", ""))
+        revision = utils.trim(push_conf.get("revision", ""))
+        filename = utils.trim(push_conf.get("filename", ""))
+
+        if not username or not gistid or filename:
+            return ""
+
+        prefix = f"https://gist.githubusercontent.com/{username}/{gistid}"
+        if revision:
+            return f"{prefix}/{revision}/{filename}"
+
+        return f"{prefix}/{filename}"
+
+
+PUSHTYPE = Enum(
+    "PUSHTYPE",
+    (
+        "paste.ding.free.hr",
+        "pastefy.ga",
+        "paste.gg",
+        "imperialb.in",
+        "gist.githubusercontent.com",
+    ),
+)
 
 
 def get_instance() -> PushTo:
@@ -403,4 +477,6 @@ def get_instance() -> PushTo:
         return PushToPasteGG(token=token)
     elif push_type == 4:
         return PushToImperial(token=token)
+    elif push_type == 5:
+        return PushToGist(token=token)
     return PushToLocal()
