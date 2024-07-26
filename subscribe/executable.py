d38fe530eb036e6b26f2f09bdb9174b7ccd65083
wzdnzd <hanxi2014@gmail.com>
2024-07-26 16:55:55 +0800

    change the naming format of executable files
    Former-commit-id: eca799e75e2d841772e499cbe83937c84c2749b6

--- subscribe/executable.py
+++ subscribe/executable.py
@@ -5,22 +5,37 @@ from logger import logger
 
 
 def which_bin() -> tuple[str, str]:
-    operating_system = str(platform.platform())
-    if operating_system.startswith("macOS"):
-        if "arm64" in operating_system:
-            clashname = "clash-darwin-arm"
-        else:
-            clashname = "clash-darwin-amd"
-
-        subconverter = "subconverter-darwin"
-    elif operating_system.startswith("Linux"):
-        clashname = "clash-linux"
-        subconverter = "subconverter-linux"
-    elif operating_system.startswith("Windows"):
-        clashname = "clash-windows.exe"
-        subconverter = "subconverter-windows.exe"
-    else:
-        logger.error("Unsupported Platform")
+    cpu_arch = get_cpu_architecture()
+    if cpu_arch not in ["amd", "arm"]:
+        logger.error(f"subconverter does not support current cpu architecture: {cpu_arch}")
         sys.exit(1)
 
+    operating_system = platform.system()
+    if operating_system not in ["Windows", "Linux", "Darwin"]:
+        logger.error(f"subconverter does not support current operating system: {operating_system}")
+        sys.exit(1)
+
+    if operating_system == "Windows" and cpu_arch != "amd":
+        logger.error(f"the windows version of subconverter only supports amd64 architecture, current: {cpu_arch}")
+        sys.exit(1)
+
+    clashname = f"clash-{operating_system.lower()}-{cpu_arch}"
+    subconverter = f"subconverter-{operating_system.lower()}-{cpu_arch}"
+
+    if operating_system == "Windows":
+        clashname += ".exe"
+        subconverter += ".exe"
+
     return clashname, subconverter
+
+
+def get_cpu_architecture() -> str:
+    machine = platform.machine()
+    if "x86_64" in machine or "AMD64" in machine:
+        return "amd"
+    elif "x86" in machine or "i386" in machine or "i686" in machine:
+        return "x86"
+    elif "arm" in machine or "aarch64" in machine:
+        return "arm"
+    else:
+        return "unknown"
